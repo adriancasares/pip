f40daec552b69d09bd801789d51517301f1e1361
Gabriel Keller <gabrieljameskeller@gmail.com>
2022-10-14 18:23:26 +0000

    new file:   .dockerignore 	modified:   config/database.js 	modified:   docker-compose.yml 	modified:   package.json

--- cms/config/database.js
+++ cms/config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
     client: 'mysql',
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
+      port: env.int('DATABASE_PORT', 1338),
       database: env('DATABASE_NAME', 'strapi'),
       user: env('DATABASE_USERNAME', 'strapi'),
       password: env('DATABASE_PASSWORD', 'strapi'),
