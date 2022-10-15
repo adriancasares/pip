2fd0d21e23053ca6f5e8f211d0b01f8db40f664a
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-10-15 16:33:06 -0500

    fix mobile scaling for meetings

--- website/src/components/sections/meetings/Meeting.tsx
+++ website/src/components/sections/meetings/Meeting.tsx
@@ -17,7 +17,7 @@ export default function Meeting(props: {
 
   return (
     <div className="py-10 border-t-4 border-t-black/30 md:border-t-0 mx-auto max-w-4xl px-10 flex flex-col md:flex-row md:justify-between gap-4">
-      <div className="flex gap-8">
+      <div className="flex flex-col xs:flex-row gap-8">
         <div
           className={`select-none shrink-0 w-20 h-20 md:w-24 md:h-24 flex items-center justify-center rounded-2xl bg-gradient-to-tr ${
             status === "NEXT"
@@ -40,10 +40,12 @@ export default function Meeting(props: {
             {status === "LAST" && "Last Meeting"}
             {status === "PAST" && "Previous Meeting"}
           </p>
-          <p className="font-sans text-mono-b">{props.description}</p>
+          <p className="font-sans text-mono-b whitespace-normal">
+            {props.description}
+          </p>
         </div>
       </div>
-      <div className="flex ml-28 md:flex-col gap-4 items-end whitespace-nowrap">
+      <div className="flex xs:ml-28 md:ml-0 md:flex-col gap-4 items-end whitespace-nowrap flex-wrap">
         <p className="font-sans text-mono-b bg-mono-border py-4 px-6 rounded-full md:bg-transparent md:py-0 md:px-0 md:rounded-none">
           {localDate.toLocaleDateString("en-US", {
             month: "long",
