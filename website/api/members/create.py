866b972e526861644e568aafe18930e43453285c
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-10-21 12:09:07 -0500

    use cgi to get form data

--- website/api/members/create.py
+++ website/api/members/create.py
@@ -1,14 +1,20 @@
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
+import cgi
 
 class handler(BaseHTTPRequestHandler):
 
   def do_POST(self):
     content_length = int(self.headers['Content-Length'])
-    body = self.rfile.read(content_length)
 
-    print(body)
-    
+    form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD': 'POST'}
+    )
+
+    print(form)
+
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
