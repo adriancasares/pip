782e27326e79e5c950ffae06ff18196157ed42c0
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-10-22 15:31:15 -0500

    get cardfile from URL

--- website/api/misc/contact.ts
+++ website/api/misc/contact.ts
@@ -1,26 +1,14 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-
+import axios from "axios";
 export default async function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
+  const cardFile = (await axios.get("https://lasapip.vercel.app/Contact.vcf"))
+    .data;
+
+  console.log(cardFile);
+
   // send text/vcard
-  response
-    .status(200)
-    .setHeader("Content-Type", "text/vcard")
-    .send(
-      `BEGIN:VCARD
-        VERSION:3.0
-        PRODID:-//Apple Inc.//macOS 12.1//EN
-        N:;;;;
-        FN:Programming in Practice
-        ORG:Programming in Practice;
-        EMAIL;type=INTERNET;type=pref:help@lasapip.com
-        TEL;type=pref:+15126402455
-        item1.URL;type=pref:https://lasapip.com
-        item1.X-ABLabel:_$!<HomePage>!$_
-        X-ABShowAs:COMPANY
-        END:VCARD
-        `
-    );
+  response.status(200).setHeader("Content-Type", "text/vcard").send(cardFile);
 }
