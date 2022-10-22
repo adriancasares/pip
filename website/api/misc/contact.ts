6d99bbce2ab23dc5132c4f4393c1e2d8779de719
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-10-22 15:03:36 -0500

    add contact api path

---
+++ website/api/misc/contact.ts
@@ -0,0 +1,26 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default async function handler(
+  request: VercelRequest,
+  response: VercelResponse
+) {
+  // send text/vcard
+  response.status(200).json({
+    body: `BEGIN:VCARD
+        VERSION:3.0
+        PRODID:-//Apple Inc.//macOS 12.1//EN
+        N:;;;;
+        FN:Programming in Practice
+        ORG:Programming in Practice;
+        EMAIL;type=INTERNET;type=pref:help@lasapip.com
+        TEL;type=pref:+15126402455
+        item1.URL;type=pref:https://lasapip.com
+        item1.X-ABLabel:_$!<HomePage>!$_
+        X-ABShowAs:COMPANY
+        END:VCARD
+        `,
+    headers: {
+      "Content-Type": "text/vcard",
+    },
+  });
+}
