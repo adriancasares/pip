fb29aec92231be0c52dade02a1db6b241e78ba35
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-10-21 23:35:42 -0500

    add firebase code

--- website/api/members/create.ts
+++ website/api/members/create.ts
@@ -1,27 +1,51 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import multiparty from "multiparty";
+import { initializeApp } from "firebase/app";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 
 export default async function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    // databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
   const form = new multiparty.Form();
 
-  let FormResp = await new Promise((resolve, reject) => {
+  let FormResp: any = await new Promise((resolve, reject) => {
     form.parse(request, (err, fields, files) => {
       if (err) reject(err);
       resolve({ fields, files });
     });
   });
 
-  // get firstName fielf from form
   const firstName = FormResp.fields.firstName[0];
+  const lastName = FormResp.fields.lastName[0];
+  const email = FormResp.fields.email[0];
+  const phone = FormResp.fields.phone[0];
+  const classYear = FormResp.fields.classYear[0];
+  const preference = FormResp.fields.preference[0];
 
-  // print the json data
-  console.log(firstName);
+  console.log(firstName, lastName, email, phone, classYear, preference);
 
-  console.log(request.body);
-  console.log(form);
+  const docRef = await addDoc(collection(db, "members"), {
+    firstName,
+    lastName,
+    email,
+    phone,
+    classYear,
+    preference,
+  });
 
   response.status(200).send("success");
 }
