9a658da5ecdb41baa35838514fd49f3452243b52
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-10-29 11:44:01 -0500

    add db config for heroku

--- cms/config/database.js
+++ cms/config/database.js
@@ -1,16 +1,19 @@
-const path = require('path');
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 1338),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
       ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        rejectUnauthorized: false,
       },
     },
     debug: false,
