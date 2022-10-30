cc9b9e46fd723593994495b6eb1dec33ff822b21
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-10-29 21:25:25 -0500

    remove parsing from onRecieve

--- website/api/messaging/webhook/onRecieve.ts
+++ website/api/messaging/webhook/onRecieve.ts
@@ -6,37 +6,39 @@ export default async function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
-  const form = new multiparty.Form();
+  //   const form = new multiparty.Form();
 
-  let FormResp: any = await new Promise((resolve, reject) => {
-    form.parse(request, (err, fields, files) => {
-      if (err) reject(err);
-      resolve({ fields, files });
-    });
-  });
+  //   let FormResp: any = await new Promise((resolve, reject) => {
+  //     form.parse(request, (err, fields, files) => {
+  //       if (err) reject(err);
+  //       resolve({ fields, files });
+  //     });
+  //   });
 
-  const CallSid = FormResp.fields.CallSid[0];
-  const Caller = FormResp.fields.Caller[0];
-  const Digits = FormResp.fields.Digits[0];
-  const From = FormResp.fields.From[0];
-  const To = FormResp.fields.To[0];
+  //   const CallSid = FormResp.fields.CallSid[0];
+  //   const Caller = FormResp.fields.Caller[0];
+  //   const Digits = FormResp.fields.Digits[0];
+  //   const From = FormResp.fields.From[0];
+  //   const To = FormResp.fields.To[0];
 
-  const params = {
-    To,
-    From,
-    Digits,
-    CallSid,
-    Caller,
-  };
+  //   const params = {
+  //     To,
+  //     From,
+  //     Digits,
+  //     CallSid,
+  //     Caller,
+  //   };
 
-  console.log(params);
+  //   console.log(params);
 
-  //get twilio signature from header
-  const signature = request.headers["X-Twilio-Signature"];
+  //   //get twilio signature from header
+  //   const signature = request.headers["X-Twilio-Signature"];
 
-  console.log("signature: " + signature);
+  //   console.log("signature: " + signature);
 
-  console.log(params);
+  //   console.log(params);
+
+  console.log(request);
 
   response.status(200).send("OK");
 }
