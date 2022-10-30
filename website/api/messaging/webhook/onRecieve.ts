27f1c1c0b7aaf117894708b59b8efc00dd866127
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-10-29 21:59:54 -0500

    update response messages

--- website/api/messaging/webhook/onRecieve.ts
+++ website/api/messaging/webhook/onRecieve.ts
@@ -1,48 +1,40 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import twilio from "twilio";
 import multiparty from "multiparty";
+import dotenv from "dotenv";
+import phoneChecker from "phone";
+import admin from "firebase-admin";
+import { getFirestore, CollectionReference } from "firebase-admin/firestore";
 
 export default async function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
-  //   const form = new multiparty.Form();
+  dotenv.config();
 
-  //   let FormResp: any = await new Promise((resolve, reject) => {
-  //     form.parse(request, (err, fields, files) => {
-  //       if (err) reject(err);
-  //       resolve({ fields, files });
-  //     });
-  //   });
+  const firebaseConfig = {
+    type: "service_account",
+    project_id: "lasapip",
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  };
 
-  //   const CallSid = FormResp.fields.CallSid[0];
-  //   const Caller = FormResp.fields.Caller[0];
-  //   const Digits = FormResp.fields.Digits[0];
-  //   const From = FormResp.fields.From[0];
-  //   const To = FormResp.fields.To[0];
+  const app = admin.apps.length
+    ? admin.app()
+    : admin.initializeApp({
+        // @ts-ignore
+        credential: admin.credential.cert(firebaseConfig),
+      });
 
-  //   const params = {
-  //     To,
-  //     From,
-  //     Digits,
-  //     CallSid,
-  //     Caller,
-  //   };
+  const db = getFirestore(app);
 
-  //   console.log(params);
-
-  //   //get twilio signature from header
-  //   const signature = request.headers["X-Twilio-Signature"];
-
-  //   console.log("signature: " + signature);
-
-  //   console.log(params);
-
-  console.log(request);
-
-  console.log(request.body);
-
-  const validation = twilio.validateRequest(
+  const valid = twilio.validateRequest(
     process.env.TWILIO_AUTH_TOKEN || "",
     // @ts-ignore
     request.headers["x-twilio-signature"],
@@ -50,7 +42,37 @@ export default async function handler(
     request.body
   );
 
-  console.log(validation);
+  if (!valid) {
+    response.status(401).send("Unauthorized");
+    return;
+  }
 
+  const from = phoneChecker(request.body.From).phoneNumber;
+
+  const message = request.body.Body;
+
+  const memberStatus = await db
+    .collection("member")
+    .where("phone", "==", from)
+    .get();
+
+  if (memberStatus.empty) {
+    const twiml = new twilio.twiml.MessagingResponse();
+
+    twiml.message(
+      "🤖 Programming in Practice:\nYou're not a member. Want texts?\nhttps://www.lasapip.com/add-phone"
+    );
+
+    response.status(200).send(twiml.toString());
+  } else if (message.toLowerCase() === "stop") {
+    const twiml = new twilio.twiml.MessagingResponse();
+
+    twiml.message(
+      "🤖 Programming in Practice:\nYou've been unsubscribed from messages. Want to subscribe again? Text us anything."
+    );
+  }
+
+  //   if(message.toLowerCase() === "stop"){
+  //     response.status(200).send("You have been unsubscribed from all messages. If you would like to subscribe again, please text 'subscribe'.");
   response.status(200).send("OK");
 }
