9584d4f494d71a9a55bd6dda25d193b755cfce6f
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-11-06 11:14:53 -0600

    change meetings rendering

--- website/src/components/sections/meetings/Meetings.tsx
+++ website/src/components/sections/meetings/Meetings.tsx
@@ -3,26 +3,34 @@ import PreviousMeetings from "./PreviousMeetings";
 import MeetingHeader from "./MeetingHeader";
 
 export default function Meetings(props: { meetings: MeetingData[] }) {
-	const meetings = props.meetings.sort((a, b) => {
-		return Date.parse(b.date) - Date.parse(a.date);
-	});
-	
-	const isUpcoming = (date: string) => {
-		return Date.parse(date) > Date.now();
-	}
+  const meetings = props.meetings.sort((a, b) => {
+    return Date.parse(b.date) - Date.parse(a.date);
+  });
 
-	return <section id="header" class="w-full bg-section-bg-purple py-20 flex flex-col gap-10">
+  const isUpcoming = (date: string) => {
+    return Date.parse(date) > Date.now();
+  };
 
-				<MeetingHeader>Meetings</MeetingHeader>
-					
-				<div class="flex flex-col w-fit mx-auto">
-						
-						{ meetings.splice(0, 2).map((meeting, idx) => {
-							return <Meeting upcoming={isUpcoming(meeting.date)} {...meeting} />
-						})}
+  return (
+    <section
+      id="header"
+      className="w-full bg-section-bg-purple py-20 flex flex-col gap-10"
+    >
+      <MeetingHeader>Meetings</MeetingHeader>
 
-					<PreviousMeetings meetings={meetings} />
+      <div className="flex flex-col w-fit mx-auto">
+        {meetings.slice(0, 2).map((meeting, idx) => {
+          return (
+            <Meeting
+              key={idx}
+              upcoming={isUpcoming(meeting.date)}
+              {...meeting}
+            />
+          );
+        })}
 
-				</div>
-			</section>;
+        <PreviousMeetings meetings={meetings.slice(2)} />
+      </div>
+    </section>
+  );
 }
